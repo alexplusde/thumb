1ea4b22b11eb41846092de0c9980a28d1db60569
Alexander Walther <mail@alexplus.de>
2024-09-06 20:10:55 +0200

    Link direkt zu den Anbietern

--- pages/media_manager.thumb.settings.php
+++ pages/media_manager.thumb.settings.php
@@ -11,21 +11,21 @@ $select->addOption('hc2i.io', 'hc2i');
 $select->addOption('html2image.net', 'h2in');
 
 $field = $form->addInputField('text', 'hcti_username', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('thumb_config_hcti_username_label'));
-$field->setNotice(rex_i18n::msg('thumb_config_hcti_username_notice'));
+$field->setLabel(rex_i18n::rawMsg('thumb_config_hcti_username_label'));
 
 $field = $form->addInputField('text', 'hcti_api_key', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('thumb_config_hcti_api_key_label'));
-$field->setNotice(rex_i18n::msg('thumb_config_hcti_api_key_notice'));
+$field->setLabel(rex_i18n::rawMsg('thumb_config_hcti_api_key_label'));
+$field->setNotice(rex_i18n::rawMsg('thumb_config_hcti_api_key_notice'));
 
 $field = $form->addInputField('text', 'h2in_api_key', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('thumb_config_h2in_api_key_label'));
-$field->setNotice(rex_i18n::msg('thumb_config_h2in_api_key_notice'));
+$field->setLabel(rex_i18n::rawMsg('thumb_config_h2in_api_key_label'));
+$field->setNotice(rex_i18n::rawMsg('thumb_config_h2in_api_key_notice'));
 
 $field = $form->addSelectField('media_manager_profile', $value = null, ['class'=>'form-control selectpicker']);
 $field->setLabel(rex_i18n::msg('thumb_config_media_manager_profile_label'));
 $field->setNotice(rex_i18n::msg('thumb_config_media_manager_profile_notice'));
 $select = $field->getSelect();
+
 // Get available media manager profiles
 $query = 'SELECT id, status, name FROM ' . rex::getTablePrefix() . 'media_manager_type ORDER BY status, name';
 
@@ -36,7 +36,7 @@ foreach ($profiles as $profile) {
 
 /* Select Fragment html.php or svg.php */
 $field = $form->addSelectField('fragment', $value = null, ['class'=>'form-control']);
-$field->setLabel(rex_i18n::msg('thumb_config_fragment_label'));
+$field->setLabel(rex_i18n::rawMsg('thumb_config_fragment_label'));
 $select = $field->getSelect();
 $select->addOption('thumb/html.php', 'thumb/html.php');
 $select->addOption('thumb/svg.php', 'thumb/svg.php');
