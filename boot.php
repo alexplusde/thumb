0ba47c723527b5ca34f2d82152d9f4317dd5aee8
Alexander Walther <mail@alexplus.de>
2024-09-06 19:02:25 +0200

    merge Structure-EPs for deleting cached Title files

--- boot.php
+++ boot.php
@@ -5,16 +5,14 @@ use Url\Url;
 use Url\Seo;
 
 if (rex::isBackend()) {
-    /* TODO: Wenn sich ein Artikel oder das Background-Medium ändert, dann entsprechend gecachte Bilder invalidieren */
-
-    rex_extension::register('ART_UPDATED', Thumb::EpArtUpdated(...), rex_extension::LATE);
-    // rex_extension::register('ART_META_UPDATED', ['thumb','ep_art_meta_updated'], rex_extension::LATE);
-    rex_extension::register('CAT_UPDATED', Thumb::EpCatUpdated(...), rex_extension::LATE);
+    /* TODO: Wenn sich ein Artikel oder das Background-Medium ändert, oder Metadaten, oder überhaupt, dann entsprechend gecachte Bilder invalidieren */
+    rex_extension::register('ART_UPDATED', Thumb::epStructureUpdated(...), rex_extension::LATE);
+    rex_extension::register('CAT_UPDATED', Thumb::epStructureUpdated(...), rex_extension::LATE);
 }
 
 if (rex::isFrontend() && rex_addon::get('url')->isAvailable()) {
     rex_extension::register('URL_SEO_TAGS', Thumb::EpSeoTags(...), rex_extension::LATE);
 } else if (rex::isFrontend() && rex_addon::get('yrewrite')->isAvailable()) {
     rex_extension::register('YREWRITE_SEO_TAGS', Thumb::EpSeoTags(...), rex_extension::LATE);
-
 }
+rex_extension::register('YREWRITE_SEO_TAGS', Thumb::EpSeoTags(...), rex_extension::LATE);
